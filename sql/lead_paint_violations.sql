e55683a5f8b1d71ccdba75d995f7651ca0913a41
ziggy <ziggy@elephant-bird.net>
2018-06-22 09:39:43 -0400

    include 2018 in lead paint violations

--- sql/lead_paint_violations.sql
+++ sql/lead_paint_violations.sql
@@ -2,4 +2,4 @@ SELECT distinct bbl
 FROM hpd_violations
 WHERE novdescription like '%LEAD-BASED PAINT HAZARD%'
       AND "class" = 'C'
-      AND date_part('year', novissueddate) in (2015,2016,2017)
+      AND date_part('year', novissueddate) in (2015,2016,2017,2018)
